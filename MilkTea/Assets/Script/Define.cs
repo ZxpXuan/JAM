b8052e134d6ba7f4bb549f51124613774ba187e8
ZxpXuan <38170958+ZxpXuan@users.noreply.github.com>
2018-06-02 20:32:28 +0800

    1

--- MilkTea/Assets/Script/Define.cs
+++ MilkTea/Assets/Script/Define.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Define {
 
     public static float selfDamagePerAttack = 3f;//发射时扣的血
-    public static float selfDamagePerAttackA1 = 3f;//子弹携带的力度a1
+    public static float selfDamagePerAttackA1 = 0.5f;//子弹携带的力度a1
     public static float selfDamagePerAttackA2 = 3f;//子弹携带的力度a2
 
     public static float t_max = 3f;
     public static float longPressA1 = 1f;
-    public static float longPressA2 = 1f;
+    public static float longPressA2 = 0.1f;
 
 }
