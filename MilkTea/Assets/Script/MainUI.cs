dc95f74146359d0a945d8bfe5ec827858e82a5e4
a66740760 <heshengchao@bupt.edu.cn>
2018-06-02 00:02:13 -0700

    1111

---
+++ MilkTea/Assets/Script/MainUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainUI : MonoBehaviour {
+	[SerializeField]
+	private GameObject SettingPanel;
+	[SerializeField]
+	private GameObject MainPanel;
+	// Use this for initialization
+	void Start () {
+		ShowMainMenu ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void OnStarBtnDown(){
+		Debug.Log("Click start");
+		Application.LoadLevel (1);
+	}
+	public void OnEndBtnDown(){
+		Application.Quit ();
+		Debug.Log("Click end");
+	}
+	private void ShowMainMenu(){
+		MainPanel.SetActive (true);
+	}
+
+}
