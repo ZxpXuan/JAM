7364021336fd4ff0206874ccfc13ce348af29b8c
ZxpXuan <xuanyuning@foxmail.com>
2018-06-03 10:45:48 +0800

    数值调整，新增角色prefab

--- MilkTea/Assets/Script/Define.cs
+++ MilkTea/Assets/Script/Define.cs
@@ -6,7 +6,7 @@ public class Define {
 
     public static float selfDamagePerAttack = 3f;//发射时扣的血
     public static float selfDamagePerAttackA1 = 0.5f;//子弹携带的力度a1
-    public static float selfDamagePerAttackA2 = 3f;//子弹携带的力度a2
+    public static float selfDamagePerAttackA2 = 1.5f;//子弹携带的力度a2
 
     public static float t_max = 3f;
     public static float longPressA1 = 1f;
@@ -17,7 +17,7 @@ public class Define {
     public static float knockDownDamage = 30f;//角色被击倒后，固定损失HP：HP_Fall_Cut
 
 
-    public static float strawMaxAngle = 45f;//吸管最大角度
+    public static float strawMaxAngle = 30f;//吸管最大角度
 
     public static float shortPressStrength = 3f;//短按固定力
     
